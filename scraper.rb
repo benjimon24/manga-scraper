c249c530f7465d587283261764cc8e381ab9a20e
Benjamin Li <benji102491@gmail.com>
2017-09-05 05:25:18 -0400

    fix problem with invalid queries by encoding the url

--- scraper.rb
+++ scraper.rb
@@ -3,7 +3,7 @@ require 'open-uri'
 require 'fileutils'
 require_relative 'zip'
 require 'openssl'
-# require 'pry'
+
 OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
 def request_url
@@ -22,15 +22,20 @@ def download_image(link)
   IO.copy_stream(download, "#{$title}/#{download.base_uri.to_s.split('/')[-1]}")
 end
 
+def create_temp_folder
+  begin
+    Dir.mkdir($title)
+  rescue
+    puts "Folder already exists!"
+  end
+end
+
+#Run script
+
 request_url
 $title = $page.css('h3').text
 
-begin
-  Dir.mkdir($title)
-rescue
-  puts "Folder already exists!"
-end
-
+create_temp_folder
 $link = $page.css('a[title=Read]').attr('href').value + "page/"
 
 i = 1
@@ -40,8 +45,9 @@ loop do
 
     page = Nokogiri::HTML(open(current_page))
     image_source = page.css('img').attr('src')
+    encoded_url = URI::encode(image_source.value) #this protects the scraper from invalid queries such as spaces in a url
     puts "Downloading #{current_page}..."
-    download_image(image_source)
+    download_image(encoded_url)
     i += 1
   rescue
     puts "Download Complete"
@@ -49,6 +55,7 @@ loop do
   end
 end
 
+#Zips files
 puts "Zipping files..."
 ZipFileGenerator.new($title, "#{$title}.zip").write()
 puts "Zipping Complete! Cleaning up temporary files..."
